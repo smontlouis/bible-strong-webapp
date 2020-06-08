f8eedafb140e4e3f23dd6d47bdf601c313be2223
bulby97 <s.montlouis.calixte@gmail.com>
2020-06-09 08:05:51 +1200

    Add promise

--- pages/api/export-study-pdf.ts
+++ pages/api/export-study-pdf.ts
@@ -1,14 +1,22 @@
 const puppeteer = require('puppeteer')
 
 export default async (req, res) => {
-  const { studyId } = req.body
-  const browser = await puppeteer.launch({ headless: true })
-  const page = await browser.newPage()
-  await page.goto(`https://bible-strong-web-app.now.sh/studies/${studyId}`, {
-    waitUntil: 'networkidle0',
-  })
-  const buffer = await page.pdf({ format: 'A4' })
+  return new Promise((resolve) => {
+    ;(async () => {
+      const { studyId } = req.body
+      const browser = await puppeteer.launch({ headless: true })
+      const page = await browser.newPage()
+      await page.goto(
+        `https://bible-strong-web-app.now.sh/studies/${studyId}`,
+        {
+          waitUntil: 'networkidle0',
+        }
+      )
+      const buffer = await page.pdf({ format: 'A4' })
 
-  await browser.close()
-  res.end(buffer)
+      await browser.close()
+      res.end(buffer)
+      return resolve()
+    })()
+  })
 }
