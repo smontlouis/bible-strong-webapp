bef7ebda34667632f2154cd9b240b17638fd3cf2
Stéphane Montlouis-Calixte <s.montlouis.calixte@gmail.com>
2020-11-30 18:26:51 +0100

    Fix firebase persistence

--- lib/firebase-app.ts
+++ lib/firebase-app.ts
@@ -7,16 +7,16 @@ import config from '../config'
 try {
   firebase.initializeApp(config.firebase)
 
-  firebase
-    .firestore()
-    .enablePersistence()
-    .catch(function (err) {
-      if (err.code == 'failed-precondition') {
-        console.log(err)
-      } else if (err.code == 'unimplemented') {
-        console.log(err)
-      }
-    })
+  // firebase
+  //   .firestore()
+  //   .enablePersistence()
+  //   .catch(function (err) {
+  //     if (err.code == 'failed-precondition') {
+  //       console.log(err)
+  //     } else if (err.code == 'unimplemented') {
+  //       console.log(err)
+  //     }
+  //   })
 } catch (error) {
   if (!/already exists/u.test(error.message)) {
     console.error('Firebase admin initialization error', error.stack)
