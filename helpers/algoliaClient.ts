f42470beaa41f28ebba7dbc795d171f5a8a9236f
Dan Liebault <d.liebault@systancia.com>
2024-08-05 12:12:29 +0200

    delete old app

--- helpers/algoliaClient.ts
+++
@@ -1,6 +0,0 @@
-import algoliasearch from 'algoliasearch/lite'
-
-export const algoliaClient = algoliasearch(
-  '62B07GBMQM',
-  '3b523859f9d0cf2aed1375607bfdfb7a'
-)
--- next-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-declare module '*.svg' {
-  const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>
-  export = value
-}
-
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next-images" />
