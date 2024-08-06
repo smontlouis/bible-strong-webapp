7b84744ea16885f34440cdd34a261dfc176f15d4
Dan Liebault <d.liebault@systancia.com>
2024-08-06 14:58:14 +0200

    Refactor login page with improved styling and form layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
+import { Box, Center, Image, Text } from '@chakra-ui/react';
 
 import LoginWidget from '@/features/auth/LoginWidget';
 import { firebaseapp, ui_config } from '@/lib/firebase-app';
@@ -8,7 +9,13 @@ import { firebaseapp, ui_config } from '@/lib/firebase-app';
 export default function LoginPage() {
     return (
         <main>
-            <LoginWidget client={firebaseapp} config={ui_config} />
+            <Center h="100vh" flexDir='column'>
+                <Image src='/images/logo-full.svg' w="500px" h="auto" />
+                <Text mt="l">Connectez-vous</Text>
+                <Box minW={300} mt="s">
+                    <LoginWidget client={firebaseapp} config={ui_config} />
+                </Box>
+            </Center>
         </main>
     )
 }
