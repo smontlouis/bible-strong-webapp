63a77d844fe7acf12a7a9ac59aae2feff7a80b23
Dan Liebault <dan.liebault@viacesi.fr>
2024-08-25 18:00:27 +0200

    chore: Update Firebase configuration to use environment variables

--- config.ts
+++ config.ts
@@ -1,12 +1,14 @@
-export default {
+const config = {
     firebase: {
-        apiKey: 'AIzaSyD4xDXoaA-TFepG-dzCO7mkY0MoIPL2ym4',
-        authDomain: 'bible-strong-app.firebaseapp.com',
-        databaseURL: 'https://bible-strong-app.firebaseio.com',
-        projectId: 'bible-strong-app',
-        storageBucket: 'bible-strong-app.appspot.com',
-        messagingSenderId: '204116128917',
-        appId: '1:204116128917:web:6ec6a6562ad7957402579c',
-        measurementId: 'G-JGJGH2BDW8',
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     },
-}
+};
+
+export default config;
