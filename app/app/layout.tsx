ba80b3b25076aa15cc60f3d617e8ef30ca6ac597
Dan Liebault <d.liebault@systancia.com>
2024-08-28 12:40:57 +0200

    prototype main navbar layout

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,47 +1,67 @@
 'use client';
 
 import Link from 'next/link';
-import { PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 
 import { FiGlobe, FiSend, FiHelpCircle, FiGithub, FiFacebook, FiDollarSign, FiShare2 } from 'react-icons/fi';
 import './navbar.layout.scss';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { firebase_app } from '@/lib/firebase-app';
 
 const Layout = ({ children }: PropsWithChildren<{}>) => {
     const auth = getAuth(firebase_app);
     const user = auth.currentUser;
 
+    const [display_name, setDisplayName] = useState<string>('');
+    const [photo_url, setPhotoUrl] = useState<string>('/images/avatar-dummy.png');
+
+    useEffect(() => {
+        if (user) {
+            setPhotoUrl(user.photoURL || '/images/avatar-dummy.png');
+            setDisplayName(user.displayName || '');
+        }
+    }, []);
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setPhotoUrl(user.photoURL || '/images/avatar-dummy.png');
+            setDisplayName(user.displayName || '');
+        }
+        else {
+            // User is signed out
+        }
+    });
+
     return (
         <>
             <header id='nav-container'>
                 <nav className='navbar'>
                     <section className='profil'>
+                        <img src={photo_url} alt="Avatar" />
                         <article id="user">
-                            <img src="/images/avatar-dummy.png" alt="Avatar" />
-                                <h2>Bonjour {user?.displayName}</h2>
-                                <div>
+                            <h2>Bonjour {user?.displayName}</h2>
+                            <div>
                                 <legend>Verset du jour</legend>
                                 <p>Le Seigneur est fidèle, il vous affermira et vous preservera du malin.</p>
-                                </div>
+                            </div>
                         </article>
                     </section>
                     <section className='settings' id="settings">
                         <ul>
                             <li>
-                                <FiHelpCircle />
+                                <FiHelpCircle size={20} />
                                 <Link href="/">Foire aux questions</Link>
                             </li>
                             <li>
-                                <FiSend />
+                                <FiSend size={20} />
                                 <Link href="/">Contacter le développeur</Link>
                             </li>
                             <li>
-                                <FiGlobe />
+                                <FiGlobe size={20} />
                                 <Link href="/">Changer la langue</Link>
                             </li>
                             <li>
-                                <FiDollarSign />
+                                <FiDollarSign size={20} />
                                 <Link href="/">Contribuer</Link>
                             </li>
                         </ul>
