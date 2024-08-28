54fae6ec8b5f0d72d413f5e9e599f1dba32910c0
Dan Liebault <d.liebault@systancia.com>
2024-08-28 18:50:30 +0200

    Display bible chapter & bible navigation & line break style

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,25 +1,48 @@
 'use client';
 
 import React, { useCallback, useEffect, useState } from 'react';
-// import Browser from './browser';
+
 import { collection, query, where, getDocs, getFirestore } from 'firebase/firestore';
 import { firebase_app } from '@/lib/firebase-app';
+import { Verse } from '@/lib/types/bible';
 
 import './page.scss';
 
-type Verse = { // firebase verse object
-    id: string;
-    book: number;
-    chapter: number;
-    verse: number;
-    content: string;
-};
+import books_map from '@/lib/types/books.json';
 
 type Index = { // where the user wants to navigates
     book: number;
     chapter: number;
     verse: number;
 };
+
+type StyleSettings = {
+    line_break: boolean;
+};
+
+function renderChaptersList(book: number) {
+    const book_chapters = books_map.find(b => b.numero === book)?.chapters;
+    
+    if (book_chapters) {
+        const list = [];
+
+        for (let i = 1; i <= book_chapters; i++) {
+            list.push(<option key={i} value={i}>{i}</option>);
+        }
+
+        return list;
+    }
+
+    return (<option value=''></option>);
+}
+
+function renderBooksList() {
+    return books_map.map((book, index) => {
+        return (
+            <option key={index} value={book.numero}>{book.name}</option>
+        )
+    });
+}
 
 function renderChapter(chapter: Verse[]) {
     return chapter.map((v, index) => {
@@ -32,6 +55,7 @@ function renderChapter(chapter: Verse[]) {
 const AppPage = () => {
     const [index, setIndex] = useState<Index>({ book: 1, chapter: 1, verse: 1 }); // set default index
     const [chapter, setChapter] = useState<Verse[]>([]);
+    const [style, setStyle] = useState<StyleSettings>({ line_break: false });
 
     const query_chapter = async () => {
         try {
@@ -52,13 +76,44 @@ const AppPage = () => {
     }
 
     useEffect(() => {
+        console.log(index);
         query_chapter();
-    }, [chapter]);
+    }, [index]);
+
+    const onBookChange = (e: any) => {
+        const book = Number(e.target.value);
+        const book_chapters = books_map.find(b => b.numero === book)?.chapters || 1;
+
+        if (index.chapter > book_chapters)
+            setIndex({ ...index, book: book, chapter: 1 });
+        else
+            setIndex({ ...index, book: book });
+    }
+
+    const onChapterChange = (e: any) => {
+        setIndex({ ...index, chapter: Number(e.target.value) });
+    }
 
     return (
         <main>
-            <section>
-                <p>{renderChapter(chapter)}</p>
+            <header id='index-nav'>
+                <section>
+                    <select name='book' value={index.book} onChange={onBookChange}>
+                        { renderBooksList() }
+                    </select>
+                    <select name='chapter' value={index.chapter} onChange={onChapterChange}>
+                        { renderChaptersList(index.book) }
+                    </select>
+                </section>
+                <section>
+                    <div>
+                        <input type='checkbox' name='line-break' onChange={() => setStyle({ line_break: !style.line_break })} />
+                        <label>Line Break</label>
+                    </div>
+                </section>
+            </header>
+            <section id='content'>
+                <p className={style.line_break ? 'line-break' : ''}>{renderChapter(chapter)}</p>
             </section>
         </main>
     )
---
+++ lib/types/bible.ts
@@ -0,0 +1,7 @@
+export type Verse = { // firebase verse object
+    id: string;
+    book: number;
+    chapter: number;
+    verse: number;
+    content: string;
+};
