0d5020adb05ebbc755101398cdef185c46a2ace5
Dan Liebault <dan.liebault@viacesi.fr>
2024-10-27 19:39:30 +0100

    Redirect to /app if login is successful

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,28 +2,30 @@
 
 import { Box, Center, Image, Text } from '@chakra-ui/react';
 import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 import { firebase_app } from '@/lib/firebase-app';
 import './page.scss';
 
-function connectWithGoogle() {
+function connectWithGoogle(router: AppRouterInstance) {
     const provider = new GoogleAuthProvider();
     const auth = getAuth(firebase_app);
     signInWithPopup(auth, provider)
         .then((result) => {
-            console.log(result);
+            router.push('/app')
         })
         .catch((error) => {
             console.error(error);
         });
 }
 
-function connectWithFacebook() {
+function connectWithFacebook(router: AppRouterInstance) {
     const provider = new FacebookAuthProvider();
     const auth = getAuth(firebase_app);
     signInWithPopup(auth, provider)
         .then((result) => {
-            console.log(result);
+            router.push('/app')
         })
         .catch((error) => {
             console.error(error);
@@ -31,14 +33,16 @@ function connectWithFacebook() {
 }
 
 export default function LoginPage() {
+    const router = useRouter()
+
     return (
         <main>
             <Center h="100vh" flexDir='column'>
                 <Image src='/images/logo-full.svg' w="500px" h="auto" />
                 <Text mt="l">Connectez-vous</Text>
                 <section id="oauth-div">
-                    <button id="google-button" onClick={connectWithGoogle}>Google</button>
-                    <button id="facebook-button" onClick={connectWithFacebook}>Facebook</button>
+                    <button id="google-button" onClick={() => connectWithGoogle(router)}>Google</button>
+                    <button id="facebook-button" onClick={() => connectWithFacebook(router)}>Facebook</button>
                 </section>
             </Center>
         </main>
